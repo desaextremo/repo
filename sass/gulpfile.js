cf2da0bc2afd017878765ccb9a517e4a18fe17fc
desaextremo <covalle@desarrolloextremo.com.co>
2015-06-20 10:02:47 -0500

    clase 20/06/2015

--- sass/gulpfile.js
+++ sass/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('sass/primero.scss')
+    gulp.src('sass/*.scss')
         .pipe(sass({
             errLogToConsole: true
         }))
@@ -11,5 +11,5 @@ gulp.task('styles', function() {
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/primero.scss',['styles']);
+    gulp.watch('sass/*.scss',['styles']);
 });
